58cb46138606ee36bcf86df1030b2cea40ab9d70
Arghya Sarkar <arghya.sarkar@yandex.com>
2025-06-20 14:47:27 +0530

    update

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- to disable the cmd line when idle
+vim.opt.cmdheight = 0
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
