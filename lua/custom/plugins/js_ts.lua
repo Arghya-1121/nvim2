00b130c2263bcea08a78026b58ec952b4eca474e
Arghya Sarkar <arghya1121@gmail.com>
2025-09-17 10:19:18 +0530

    little bit change

--- lua/custom/plugins/js_ts.lua
+++ lua/custom/plugins/js_ts.lua
@@ -2,50 +2,50 @@
 return {
   -- LSP Support for JS/TS
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     opts = {
       servers = {
-        tsserver = {}, -- Enables support for JavaScript and TypeScript
+        ts_ls = {}, -- Enables support for JavaScript and TypeScript
       },
     },
   },
 
   -- Treesitter parsers for JS/TS
   {
-    "nvim-treesitter/nvim-treesitter",
+    'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "javascript",
-        "typescript",
-        "tsx", -- for React / JSX / TSX files
-        "json",
+        'javascript',
+        'typescript',
+        'tsx', -- for React / JSX / TSX files
+        'json',
       })
     end,
   },
 
   -- Formatter: Prettier
   {
-    "stevearc/conform.nvim",
+    'stevearc/conform.nvim',
     opts = {
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        json = { "prettier" },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        json = { 'prettier' },
       },
     },
   },
 
   -- Linter: ESLint via eslint_d (faster than eslint)
   {
-    "mfussenegger/nvim-lint",
+    'mfussenegger/nvim-lint',
     opts = {
       linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
       },
     },
   },
